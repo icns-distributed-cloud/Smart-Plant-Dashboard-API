7ef4f1d227ca3dfe0611fa8709cf7e4c911de2e1
hyun <seok02h@khu.ac.kr>
2021-09-06 11:36:02 +0900

    initial commit

---
+++ src/main/java/icns/smartplantdashboardapi/SmartPlantDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package icns.smartplantdashboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPlantDashboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPlantDashboardApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icns/smartplantdashboardapi/SmartPlantDashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package icns.smartplantdashboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPlantDashboardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
