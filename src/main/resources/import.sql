cc9ccbb3cb94f7fcbb782002a7dee0bb6bb2150c
hyun <seok02h@khu.ac.kr>
2021-11-24 18:41:06 +0900

    Add file service

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,3 +6,5 @@ INSERT INTO sensor_type(type_code, type_dtl, type_name, type_color_code) values(
 INSERT INTO sensor_type(type_code, type_dtl, type_name, type_color_code) values('G', '가스센서', '가스', '#622aa7');
 INSERT INTO sensor_type(type_code, type_dtl, type_name, type_color_code) values('CC', '이산화탄소 센서', 'CO2', '#9e9e9e');
 INSERT INTO sensor_type(type_code, type_dtl, type_name, type_color_code) values('C', '일산화탄소 센서', 'CO', '#795548');
+
+
